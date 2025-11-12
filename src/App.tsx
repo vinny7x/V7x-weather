f45e738b2a48f9f4a70636a669f8e631c767188e
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-11-12 15:53:17 -0300

    ✨ feat: configuração do tanstack query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Header } from "./components/Header";
+import { WeatherCard } from "./components/WeatherCard";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
-
+  const queryClient = new QueryClient();
   return (<>
     <Header />
+    <QueryClientProvider client={queryClient}>
+      <WeatherCard />
+    </QueryClientProvider>
   </>
   );
-
 }
-
 export default App;
